221cb260920c9a8b19e4a42299b16f7d8c9a1a15
Lucian S. <stoicam.lucian@gmail.com>
2025-11-08 13:16:29 +0200

    feat(functions): add New-CoreResourceGroup (idempotent RG create/update)

--- automation/functions/az-functions.ps1
+++ automation/functions/az-functions.ps1
@@ -1,1 +1,21 @@
+function New-CoreResourceGroup {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory)][string]$Name,
+    [Parameter(Mandatory)][string]$Location,
+    [Parameter()][hashtable]$Tags
+  )
+
+  $existing = Get-AzResourceGroup -Name $Name -ErrorAction SilentlyContinue
+
+  if ($existing) {
+    if ($Tags) { Set-AzResourceGroup -Name $Name -Tag $Tags | Out-Null }
+    Write-Host "RG exists: $Name"
+    return $existing
+  }
+
+  $result = New-AzResourceGroup -Name $Name -Location $Location -Tag $Tags
+  Write-Host "RG created: $Name"
+  return $result
+}
 
