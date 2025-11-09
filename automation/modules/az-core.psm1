ae522776a0a62ea6c4ba003f7d6e52a4dceb1054
Lucian S. <stoicam.lucian@gmail.com>
2025-11-09 02:16:39 +0200

    feat(storage): add core ADLS Gen2 storage account and context functions
    Added validation function for storage account names and integrated it into existing functions.

--- automation/modules/az-core.psm1
+++ automation/modules/az-core.psm1
@@ -49,6 +49,70 @@ function New-CoreStorageAccount {
 
     Write-Host "Storage created: $Name"
     return $result
+
+    function Test-CoreStorageName {
+    [CmdletBinding()] param([Parameter(Mandatory)][string]$Name)
+    if ($Name -notmatch '^[a-z0-9]{3,24}$') {
+        throw "Storage Account name must be 3-24 characters, lowercase+digits only. Given: '$Name'"
+    }
+}
+
+function Get-CoreStorageAccount {
+    [CmdletBinding()] param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$ResourceGroupName
+    )
+    Test-CoreStorageName -Name $Name
+    try {
+        return Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $Name -ErrorAction Stop
+    } catch { return $null }
+}
+
+function New-CoreStorageAccount {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$ResourceGroupName,
+        [string]$Location,
+        [hashtable]$Tags,
+        [switch]$EnableHierarchicalNamespace
+    )
+
+    Test-CoreStorageName -Name $Name
+
+    if (-not $Location) {
+        $Location = (Get-AzResourceGroup -Name $ResourceGroupName).Location
+    }
+
+    $existing = Get-CoreStorageAccount -Name $Name -ResourceGroupName $ResourceGroupName
+    if ($existing) {
+        Write-Host "Storage exists: $Name"
+        return $existing
+    }
+
+    Write-Host "Creating Storage: $Name (ADLS Gen2=$EnableHierarchicalNamespace)"
+    return New-AzStorageAccount `
+        -Name $Name `
+        -ResourceGroupName $ResourceGroupName `
+        -Location $Location `
+        -SkuName Standard_LRS `
+        -Kind StorageV2 `
+        -EnableHierarchicalNamespace:($EnableHierarchicalNamespace.IsPresent) `
+        -Tags $Tags
+}
+
+function Set-CoreStorageContext {
+    [CmdletBinding()] param(
+        [Parameter(Mandatory)][string]$Name,
+        [Parameter(Mandatory)][string]$ResourceGroupName
+    )
+    $sa = Get-CoreStorageAccount -Name $Name -ResourceGroupName $ResourceGroupName
+    $ctx = New-AzStorageContext -StorageAccountName $sa.StorageAccountName -UseConnectedAccount
+    Set-AzCurrentStorageAccount -ResourceGroupName $ResourceGroupName -Name $Name | Out-Null
+    Set-Variable -Name CoreStorageContext -Scope Script -Value $ctx -Force
+    return $ctx
+}
+
 }
 
 Export-ModuleMember -Function *-Core*
